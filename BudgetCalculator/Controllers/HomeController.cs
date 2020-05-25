05030ade8aa7190c87745b3517018c677012f8c3
Roman <orchikov97roman@gmail.com>
2020-05-25 14:32:01 +0300

    Fix some views and add About text

--- BudgetCalculator/Controllers/HomeController.cs
+++ BudgetCalculator/Controllers/HomeController.cs
@@ -19,14 +19,14 @@ namespace BudgetCalculator.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "This application allows you to keep track of your expenses and income. You should no longer take it manually, spend time on it. This budget calculator will do everything for you, and the donut chart will show you your expenses, which will be calculated automatically. You can independently create, edit and delete both operations and categories that you need for simplification and easy navigation. Please note that this application is a demo version, due to the fact that the project was carried out by one person and without funding. Creating this project, as a developer, I learned a very large and vital lesson for myself, you always need to use the GitHub so that you do not have to write first. And of course, how can I not say a few phrases about this project, I wrote it with pleasure, there were many difficulties, but it was worth it. Thanks for watching and taking the time.";
 
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact here";
 
             return View();
         }
